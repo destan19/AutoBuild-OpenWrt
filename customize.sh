530828a538db5c62af3d48df44d00c494d9854ee
Derry <destan19@126.com>
2021-01-06 16:06:06 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,11 +7,13 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168..1/g' openwrt/package/base-files/files/bin/config_generate
-cd openwrt/package
-git clone https://github.com/destan19/OpenAppFilter.git
-echo "clone appfilter....ok"
+cd openwrt
 echo "CONFIG_PACKAGE_appfilter=y" >>.config
 echo "CONFIG_PACKAGE_kmod-oaf=y" >>.config
 echo "CONFIG_PACKAGE_luci-i18n-oaf-zh-cn=y" >>.config
 echo "CONFIG_PACKAGE_luci-app-oaf=y" >>.config
-cd -
+cd package
+git clone https://github.com/destan19/OpenAppFilter.git
+echo "clone appfilter....ok"
+cd ../../
+

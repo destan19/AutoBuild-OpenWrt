23be57f2f6d38f245313903130fd882878b7c1b9
Derry <destan19@126.com>
2021-03-27 17:36:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-#sed -i 's/192.168.1.1/192.168..1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168..1/g' openwrt/package/base-files/files/bin/config_generate
 cd openwrt
 echo "CONFIG_PACKAGE_appfilter=y" >>.config
 echo "CONFIG_PACKAGE_kmod-oaf=y" >>.config
